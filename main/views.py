088f63c06b84460a1ba057a15c850c58a23d546b
jebs-tech <bungasaragih688@gmail.com>
2025-09-16 10:30:06 +0700

    Menambahkan poin 1 dan 2 dalam tugas 3

--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from .models import Product # Sesuaikan dengan nama model Anda
 
 # Create your views here.
 def show_main(request):
@@ -6,5 +9,33 @@ def show_main(request):
         'name': 'Jenisa Bunga',
         'class': 'PBP F'
     }
-
     return render(request, "main.html", context)
+
+def show_xml(request):
+     news_list = News.objects.all()
+     xml_data = serializers.serialize("xml", news_list)
+     return HttpResponse(xml_data, content_type="application/xml")
+
+def show_json(request):                                                              
+    news_list = News.objects.all()
+    json_data = serializers.serialize("json", news_list)
+    return HttpResponse(json_data, content_type="application/json")
+
+def show_xml_by_id(request, news_id):
+   try:
+       news_item = News.objects.filter(pk=news_id)
+       xml_data = serializers.serialize("xml", news_item)
+       return HttpResponse(xml_data, content_type="application/xml")
+   except News.DoesNotExist:
+       return HttpResponse(status=404)
+   
+
+def show_json_by_id(request, news_id):
+   try:
+       news_item = News.objects.get(pk=news_id)
+       json_data = serializers.serialize("json", [news_item])
+       return HttpResponse(json_data, content_type="application/json")
+   except News.DoesNotExist:
+       return HttpResponse(status=404)
+
+
