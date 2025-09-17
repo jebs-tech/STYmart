9c31c62a71ec109c708fd9715f5790b61f64c810
jebs-tech <bungasaragih688@gmail.com>
2025-09-17 10:14:55 +0700

    tugas individu 3

--- main/forms.py
+++ main/forms.py
@@ -4,4 +4,4 @@ from main.models import Product
 class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = ["name", "price", "description", "thumbnail", "category", "is_featured"]
+        fields = ['name', 'category', 'price', 'stock', 'color', 'size', 'thumbnail', 'description', 'is_featured']
