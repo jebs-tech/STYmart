910019ab78073290678aac9bba41df5377094056
jebs-tech <bungasaragih688@gmail.com>
2025-09-18 10:14:53 +0700

    tugas 4 selesai

--- main/models.py
+++ main/models.py
@@ -1,17 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 import uuid
 from django.db import models
 
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) # tambahkan ini
+
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     stock = models.IntegerField(default=0)
     color = models.CharField(max_length=50, blank=True)
     size = models.CharField(max_length=50, blank=True)
-
     thumbnail = models.URLField(blank=True, null=True)
     category = models.CharField(max_length=100)
     is_featured = models.BooleanField(default=False)
@@ -23,4 +25,4 @@ class Product(models.Model):
     def increment_views(self):
         self.views += 1
         self.save(update_fields=['views'])
-    
+
--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from main.views import register
+from main.views import login_user
+from main.views import logout_user
 
 from main.views import (
     delete_product, show_main, create_product, show_product,
@@ -14,7 +17,9 @@ urlpatterns = [
     path('create-product/', create_product, name='create_product'),
     path('product/<str:id>/', show_product, name='show_product'),
     path('delete/<int:pk>/', delete_product, name='delete_product'),
-
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('register/', register, name='register'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<str:Product_id>/', show_xml_by_id, name='show_xml_by_id'),
